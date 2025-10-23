8b8bbf0bd5c038f033cba18372266c7455986634
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-10-23 07:21:07 +0530

    23-10-25 - While loop

---
+++ PythonTutorials/vi_control_flow_statements/vi_looping_statements.py
@@ -0,0 +1,36 @@
+'''
+Created on 23-Oct-2025
+
+@author: Vivek
+
+"Reduce repetation - increase reuse" --> Easy maintenence
+
+Looping statements: 
+- Looping statements are used to execute any statement/s repeatedly.
+- Any statement/s will be executed repeatedly until a condition is fulfilled
+
+Types of Looping Statements:
+1. While loop:
+    - initial variable -- used to set initial value
+    - define the condition
+    - increment/ decrement
+    
+2. For loop
+
+'''
+
+'''
+count = 0 # initial variable
+
+while count < 5: # condition
+    print("Hello world!")
+    # count = count + 1 # increment
+    count+=1 # increment
+'''
+
+count = 5 # initial variable
+
+while count > 0: # condition
+    print("Hello world!")
+    # count = count - 1 # decrement
+    count-=1 # decrement
