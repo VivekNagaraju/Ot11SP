1dd91ecd5f6f0625348c019be161c339f1c176ba
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-10-26 16:03:29 +0530

    26-10-25

---
+++ PythonTutorials/vi_control_flow_statements/vi_cfs_exercises.py
@@ -0,0 +1,19 @@
+'''
+Created on 26-Oct-2025
+
+@author: Vivek
+
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+'''
+
+for j in range(6):
+    for i in range(6):
+        # print("*", end=" ")
+        print("* * * * * *") # This only works when there is fixed number of "*"(values) in every row
+    print() 
