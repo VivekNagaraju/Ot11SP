b40aca681a6604b2534e98d4788bd6f16db5eee8
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-12-11 08:44:37 +0530

    11-12-25

---
+++ Vi_SeleniumTutorials/vi_selenium_basics/vi_date_pickers.py
@@ -0,0 +1,29 @@
+'''
+Created on 11-Dec-2025
+
+@author: Vivek
+'''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 1. Launching the chrome browser 
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("start-maximized")
+driver = webdriver.Chrome(options)
+driver.implicitly_wait(5)
+
+# 2. Navigating to application URL
+
+driver.get("https://testautomationpractice.blogspot.com/")
+
+# 3. Enter Date in Date Picker 1
+date_picker_1 = driver.find_element(By.ID, "datepicker")
+date_picker_1.send_keys("01/01/2000")
+
+# 4. Enter Date in Date Picker 2
+date_picker_2 = driver.find_element(By.ID, "txtDate")
+# Remove readonly attribute using JavaScript
+driver.execute_script("arguments[0].removeAttribute('readonly');", date_picker_2)
+date_picker_2.send_keys("01/01/2000")
