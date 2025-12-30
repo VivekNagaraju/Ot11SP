93c14827b685e19b813e86e7959da5af272263c1
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-12-30 09:07:01 +0530

    30-12-25

--- PythonTutorials/other_adv_concepts/vi_regular_expressions.py
+++ PythonTutorials/other_adv_concepts/vi_regular_expressions.py
@@ -14,6 +14,10 @@ Regular Expressions: Defines the pattern of a string
 [0-9] --> any integer from 0 to 9
 [a-zA-Z0-9] --> aplhanumeric character
 [^a-zA-Z0-9] --> special character
+
+Quantifiers:
+
+54hvj.h67@iquesttechnologies.co.in
 '''
 import re
 
@@ -32,6 +36,22 @@ for i in matcher:
     
 print(count)
 
-'''
-'''
+
+print(re.search("\AAirtel", "Airtel_Vivek"))
+
+print("====================================")
+matcher = re.finditer("a{2,3}", "hgcabajaagaaabajaaaauehab")
+
+count=0
+for i in matcher:
+    print("Start index:", i.start())
+    print("Character:", i.group())
+
+print("====================================")
+phone_number_pattern = "\A[6-9]\d{9}$"
+print(bool(re.match(phone_number_pattern, "88619110")))
+
+print("=================email validation===================")
+email_pattern = "\A[a-zA-Z0-9.-_+]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,}$"
+print(bool(re.match(email_pattern, "nvivek.2205@gmail.co.in")))
     
