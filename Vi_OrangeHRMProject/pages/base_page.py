1ffb5010c1b9d701ce5e6d0015d177dc454b1020
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2026-01-22 08:51:51 +0530

    22-01-26

--- Vi_OrangeHRMProject/pages/base_page.py
+++ Vi_OrangeHRMProject/pages/base_page.py
@@ -9,22 +9,22 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
     def __init__(self, driver):
-        self.driver = driver
-        self.wait = WebDriverWait(self.driver, timeout=5)
+        self.__driver = driver
+        self.__wait = WebDriverWait(self.driver, timeout=5)
         
     def navigate_to_url(self, url):
-        self.driver.get(url)
+        self.__driver.get(url)
         
-    def enter_text(self, locator, text):
-        element = self.wait.until(EC.visibility_of_element_located(locator))
+    def _enter_text(self, locator, text):
+        element = self.__wait.until(EC.visibility_of_element_located(locator))
         element.send_keys(text)
         
-    def click_on(self, locator):
-        element = self.wait.until(EC.element_to_be_clickable(locator))
+    def _click_on(self, locator):
+        element = self.__wait.until(EC.element_to_be_clickable(locator))
         element.click()
         
     def get_page_url(self):
-        current_page_url = self.driver.current_url
+        current_page_url = self.__driver.current_url
         return current_page_url
         
         
--- Vi_OrangeHRMProject/pages/login_page.py
+++ Vi_OrangeHRMProject/pages/login_page.py
@@ -9,17 +9,20 @@ from selenium.webdriver.common.by import By
 class LoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.username_txtbx_locator = (By.NAME, "username")
-        self.password_txtbx_locator = (By.NAME, "password")
-        self.login_btn_locator = (By.XPATH,"//button[@type='submit']")
+        self.__username_txtbx_locator = (By.NAME, "username")
+        self.__password_txtbx_locator = (By.NAME, "password")
+        self.__login_btn_locator = (By.XPATH,"//button[@type='submit']")
         
     def enter_username(self, username):
-        self.enter_text(self.username_txtbx_locator, username)
+        # self.enter_text(self.username_txtbx_locator, username) # without encapsulation
+        self._enter_text(self.__username_txtbx_locator, username) # with encapsulation
         
     def enter_password(self, password):
-        self.enter_text(self.password_txtbx_locator, password)
+        # self.enter_text(self.password_txtbx_locator, password)
+        self._enter_text(self.__password_txtbx_locator, password)
         
     def click_on_login_btn(self):
-        self.click_on(self.login_btn_locator)
+        # self.click_on(self.login_btn_locator)
+        self._click_on(self.__login_btn_locator)
         
         
